0938c416883db5759190b8cc84247ce7853d9f28
salma27 <36279740+salma27@users.noreply.github.com>
2020-11-13 23:08:05 +0200

    search bar is added

---
+++ frontend/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,16 @@
+import react from 'react'
+import "./SearchBar.css";
+
+function SearchBar() {
+    return (
+        <>
+            <div className="searchBar">
+                <input id="search" placeholder="Search..."></input>
+                <button id="searchBtn">Search</button>
+            </div>
+        </>
+    );
+}
+
+
+export default SearchBar;
