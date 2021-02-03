e7e7dbb2acdfa9259a364eb04762008ca29aa031
salma27 <36279740+salma27@users.noreply.github.com>
2021-02-03 16:27:49 +0200

    login page updated

--- frontend/src/Utils/index.js
+++ frontend/src/Utils/index.js
@@ -1,3 +1,4 @@
 export { default as ErrorHandlingInput } from "./ErrorHandlingInput";
 export { default as Title } from "./Title";
 export { default as Button } from "./Button";
+export { default as LoginImg } from "./LoginImg";
--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,5 +1,76 @@
 import React, { useState } from "react";
 import "./Login.css";
+//import { ErrorHandlingInput, Title, Button } from "utils";
+import LoginImg from "../../utils/LoginImg";
+import { Button } from "react-bootstrap";
+import { Container, Row, Col, Form } from "react-bootstrap";
+
+function Login() {
+    const [id, setID] = useState("");
+    const [email, setEmail] = useState("");
+    function validateForm() {
+        return email.length > 0 && id.length > 7;
+    }
+
+    function submit(event) {
+        event.preventDefault();
+    }
+
+    return (
+        <>
+            <Container>
+                <Row width="100%">
+                    <Col sm={12} lg={6} md={6} width="50%">
+                        <LoginImg id="img" />
+                    </Col>
+                    <Col sm={12} lg={6} md={6} width="50%">
+                        <Container fluid id="loginForm">
+                            <Form onSubmit={submit}>
+                                <Form.Group size="lg" controlId="id">
+                                    <Form.Label>ID</Form.Label>
+                                    <Form.Control
+                                        placeholder="20--0---"
+                                        autoFocus
+                                        type="number"
+                                        value={id}
+                                        onChange={(e) => setID(e.target.value)}
+                                    />
+                                </Form.Group>
+                                <Form.Group size="lg" controlId="email">
+                                    <Form.Label>Email</Form.Label>
+                                    <Form.Control
+                                        autoFocus
+                                        type="email"
+                                        placeholder="example@stud.fci-cu.edu.eg"
+                                        value={email}
+                                        onChange={(e) =>
+                                            setEmail(e.target.value)
+                                        }
+                                    />
+                                </Form.Group>
+                                <Button
+                                    block
+                                    size="lg"
+                                    type="submit"
+                                    disabled={!validateForm()}
+                                    id="loginBtn"
+                                >
+                                    Login
+                                </Button>
+                            </Form>
+                        </Container>
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    );
+}
+
+export default Login;
+
+/*
+import React, { useState } from "react";
+import "./Login.css";
 import { ErrorHandlingInput, Title, Button } from "utils";
 import SearchBar from "../SearchBar/SearchBar";
 function LoginBox() {
@@ -72,3 +143,4 @@ function Login() {
 }
 
 export default Login;
+*/
