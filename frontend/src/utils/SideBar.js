f906388a3ebea17ed60bd606ccd5c7ccfd52bd53
salma27 <36279740+salma27@users.noreply.github.com>
2021-02-05 02:32:17 +0200

    tmp

--- frontend/src/utils/SideBar.js
+++ frontend/src/utils/SideBar.js
@@ -14,11 +14,32 @@ import {
 function SideBar() {
     return (
         <>
-            <ButtonGroup vertical id="sidebar">
-                <Button variant="secondary">Team Info.</Button>
-                <Button variant="secondary">Team Ideas</Button>
-                <Button variant="secondary">Add Idea</Button>
-            </ButtonGroup>
+            <div id="sidebar">
+                <Tab.Container width="100%" variant="secondary">
+                    <Row>
+                        <Col sm={3}>
+                            <Nav variant="secondary">
+                                <Nav.Item width="100%" id="list">
+                                    <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item width="100%" id="list">
+                                    <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                        </Col>
+                        <Col sm={9}>
+                            <Tab.Content>
+                                <Tab.Pane eventKey="first">
+                                    {/*<Sonnet />*/}
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="second">
+                                    {/*<Sonnet />*/}
+                                </Tab.Pane>
+                            </Tab.Content>
+                        </Col>
+                    </Row>
+                </Tab.Container>
+            </div>
         </>
     );
 }
