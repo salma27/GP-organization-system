5ee20efa581cd30bfb3629730e7e8bd0a9e86fd8
Sara-Samer <ss.titanic199@gmail.com>
2021-03-24 05:52:21 +0200

    fml

--- frontend/src/components/cards/index.js
+++ frontend/src/components/cards/index.js
@@ -1,2 +1,3 @@
 export {default as ProjectCard} from "./ProjectCard";
 export {default as RequestCard} from "./RequestCard";
+export {default as TeamInfoCard} from "./TeamInfoCard";
--- frontend/src/components/pages/user/StudentTeamPage.js
+++ frontend/src/components/pages/user/StudentTeamPage.js
@@ -1,6 +1,21 @@
+import {TeamInfoCard} from "components/cards";
 import React from "react";
+import "styles/dashboard.css";
 
 const StudentTeamPage = () => {
-    return <div>StudentTeamPage</div>;
+    return (
+        <div className="row">
+            <div className="col-12 col-md-6">
+                <TeamInfoCard />
+            </div>
+            <div className="col-0 col-md-6">
+                <div className="d-flex flex-column h-100 pb-4">
+                    <div className="img-container">
+                        <img className="img" src="/login_bg.svg" alt="bg" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 export default StudentTeamPage;
