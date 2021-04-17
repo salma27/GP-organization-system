f74be029a8f6b2c12f1dd10478dfcad8f5a6691b
salma27 <36279740+salma27@users.noreply.github.com>
2021-04-17 17:29:24 +0200

    fields of experience updated

--- frontend/src/components/Modals/EditProject.js
+++ frontend/src/components/Modals/EditProject.js
@@ -1,17 +1,37 @@
 import React, { useRef, useState } from "react";
 import { Modal, Badge } from "react-bootstrap";
-import { Editable } from "utils";
+import { Editable, BsButton } from "utils";
 import FieldsOfExperience from "../StudentProfile/FieldsOfExperience";
+import "../StudentProfile/FieldsOfExperience.css";
+import { Button, Form, Col, Row, Container } from "react-bootstrap";
 
 function EditProject(props) {
+    const [oneTech, setOne] = useState();
+
+    const setOneItem = (e) => setOne(e.target.value);
+
     const [title, setTitle] = useState("");
     const inputRef = useRef();
+    const [tech, setTech] = useState(props.tech);
     const technology = [
         "Machine Learning",
         "Artifical Intelligence",
         "Web Application",
         "Others",
     ];
+    const removeItem = (index) => {
+        const temp = [];
+        tech.forEach((v, i) => {
+            if (index !== i) {
+                temp.push(v);
+            }
+        });
+        setTech(temp);
+    };
+    const selected = () => {
+        if (oneTech && !tech.includes(oneTech) && oneTech !== "-1")
+            setTech([...tech, oneTech]);
+    };
     return (
         <>
             <Modal centered show={props.show} onHide={props.hide}>
@@ -39,7 +59,69 @@ function EditProject(props) {
                 </Modal.Header>
                 <Modal.Body>{props.brief_description}</Modal.Body>
                 <Modal.Footer>
-                    <FieldsOfExperience tech={technology} />
+                    <Form
+                        onSubmit={(e) => e.preventDefault()}
+                        className="w-100"
+                    >
+                        <Form.Group as={Col} controlId="formGridState">
+                            <Form.Label>Fields Of Experience:</Form.Label>
+                            <Form.Control
+                                as="select"
+                                style={{
+                                    color: "#00BFA6",
+                                    borderColor: "#00BFA6",
+                                    borderWidth: "1px",
+                                    borderStyle: "solid",
+                                }}
+                                onChange={setOneItem}
+                            >
+                                <option value="-1" id="list"></option>
+
+                                {technology.map((addField) => (
+                                    <option
+                                        id="list"
+                                        value={addField}
+                                        key={addField}
+                                    >
+                                        {addField}
+                                    </option>
+                                ))}
+                            </Form.Control>
+                        </Form.Group>
+                        <Button size="sm" id="addBtn" onClick={selected}>
+                            +
+                        </Button>
+                    </Form>
+
+                    {tech.map((t, i) => (
+                        <Badge
+                            pill
+                            style={{
+                                color: "#00BFA6",
+                                borderColor: "#00BFA6",
+                                borderWidth: "1px",
+                                borderStyle: "solid",
+                            }}
+                            className="mr-1 mb-1"
+                            key={i}
+                        >
+                            {t}
+                            <a
+                                href="#"
+                                style={{
+                                    float: "right",
+                                    marginLeft: "15px",
+                                }}
+                                size="sm"
+                                type="submit"
+                                onClick={() => removeItem(i)}
+                                variant="secondary"
+                            >
+                                x
+                            </a>
+                        </Badge>
+                    ))}
+                    {!props.tech.length && "No technologies provided"}
                 </Modal.Footer>
             </Modal>
         </>
