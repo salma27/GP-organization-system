6827d8f45bc9dbf27d4eedee049e1895d512562b
salma27 <36279740+salma27@users.noreply.github.com>
2021-04-17 20:37:28 +0200

    modal appearance

--- frontend/src/components/Modals/EditProject.js
+++ frontend/src/components/Modals/EditProject.js
@@ -37,127 +37,129 @@ function EditProject(props) {
         <>
             <Modal centered show={props.show} onHide={props.hide}>
                 <Modal.Header closeButton>
-                    <Modal.Title>
-                        <Row>
-                            <Col lg={3}>Title: </Col>
-                            <Col lg={9}>
-                                <Form.Control
-                                    className="w-100"
-                                    style={{
-                                        width: "100%",
-                                        borderColor: "#00BFA6",
-                                        borderWidth: "1px",
-                                        borderStyle: "solid",
-                                    }}
-                                    placeholder={props.title}
-                                />
-                            </Col>
-                        </Row>
-                    </Modal.Title>
+                    <Modal.Title>Edit Project</Modal.Title>
                 </Modal.Header>
+
                 <Modal.Body>
-                    <Form
-                        onSubmit={(e) => e.preventDefault()}
-                        className="w-100"
-                    >
-                        <Form.Label>Brief Description: </Form.Label>
-                        <Form.Control
-                            as="textarea"
-                            rows={3}
-                            style={{
-                                resize: "none",
-                                borderColor: "#00BFA6",
-                                borderWidth: "1px",
-                                borderStyle: "solid",
-                            }}
-                            value={description}
-                            placeholder={description}
-                            onChange={(e) => {
-                                setDescription(e.target.value);
-                            }}
-                        />
+                    <Form>
+                        <Form.Group>
+                            <Form.Label>Title:</Form.Label>
+                            <Form.Control
+                                className="w-100"
+                                style={{
+                                    width: "100%",
+                                    borderColor: "#00BFA6",
+                                    borderWidth: "1px",
+                                    borderStyle: "solid",
+                                }}
+                                placeholder={props.title}
+                            />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Brief Description: </Form.Label>
+                            <Form.Control
+                                as="textarea"
+                                rows={3}
+                                style={{
+                                    resize: "none",
+                                    borderColor: "#00BFA6",
+                                    borderWidth: "1px",
+                                    borderStyle: "solid",
+                                }}
+                                value={description}
+                                placeholder={description}
+                                onChange={(e) => {
+                                    setDescription(e.target.value);
+                                }}
+                            />
+                        </Form.Group>
+                        <Form.Group>
+                            <Row className="w-100">
+                                <label className="form-label">
+                                    Used Technologies:
+                                </label>
+                            </Row>
+                            <Row className="w-100">
+                                <Col lg={9} sm={12}>
+                                    <Form
+                                        onSubmit={(e) => e.preventDefault()}
+                                        className="w-100"
+                                    >
+                                        <Form.Control
+                                            as="select"
+                                            style={{
+                                                color: "#00BFA6",
+                                                borderColor: "#00BFA6",
+                                                borderWidth: "1px",
+                                                borderStyle: "solid",
+                                            }}
+                                            onChange={setOneItem}
+                                        >
+                                            <option
+                                                value="-1"
+                                                id="list"
+                                            ></option>
+
+                                            {technology.map((addField) => (
+                                                <option
+                                                    id="list"
+                                                    value={addField}
+                                                    key={addField}
+                                                >
+                                                    {addField}
+                                                </option>
+                                            ))}
+                                        </Form.Control>
+                                    </Form>
+                                </Col>
+                                <Col lg={3} sm={12}>
+                                    <Button
+                                        className="w-100"
+                                        size="sm"
+                                        type="submit"
+                                        id="addBtn"
+                                        onClick={selected}
+                                        style={{
+                                            backgroundColor: "#00BFA6",
+                                            color: "white",
+                                        }}
+                                    >
+                                        +
+                                    </Button>
+                                </Col>
+                            </Row>
+                        </Form.Group>
+                        {tech.map((t, i) => (
+                            <Badge
+                                pill
+                                style={{
+                                    color: "#00BFA6",
+                                    borderColor: "#00BFA6",
+                                    borderWidth: "1px",
+                                    borderStyle: "solid",
+                                }}
+                                className=" mb-1"
+                                key={i}
+                            >
+                                {t}
+                                <a
+                                    href="#"
+                                    style={{
+                                        marginLeft: "15px",
+                                    }}
+                                    size="sm"
+                                    type="submit"
+                                    onClick={() => removeItem(i)}
+                                    variant="secondary"
+                                >
+                                    x
+                                </a>
+                            </Badge>
+                        ))}
+                        {!props.tech.length && "No technologies provided"}
                     </Form>
                 </Modal.Body>
-                <Modal.Footer>
-                    <Row className="w-100">
-                        <label className="form-label">Used Technologies:</label>
-                    </Row>
-                    <Row className="w-100">
-                        <Col lg={9} sm={12}>
-                            <Form
-                                onSubmit={(e) => e.preventDefault()}
-                                className="w-100"
-                            >
-                                <Form.Control
-                                    as="select"
-                                    style={{
-                                        color: "#00BFA6",
-                                        borderColor: "#00BFA6",
-                                        borderWidth: "1px",
-                                        borderStyle: "solid",
-                                    }}
-                                    onChange={setOneItem}
-                                >
-                                    <option value="-1" id="list"></option>
 
-                                    {technology.map((addField) => (
-                                        <option
-                                            id="list"
-                                            value={addField}
-                                            key={addField}
-                                        >
-                                            {addField}
-                                        </option>
-                                    ))}
-                                </Form.Control>
-                            </Form>
-                        </Col>
-                        <Col lg={3} sm={12}>
-                            <Button
-                                className="w-100"
-                                size="sm"
-                                type="submit"
-                                id="addBtn"
-                                onClick={selected}
-                                style={{
-                                    backgroundColor: "#00BFA6",
-                                    color: "white",
-                                }}
-                            >
-                                +
-                            </Button>
-                        </Col>
-                    </Row>
-
-                    {tech.map((t, i) => (
-                        <Badge
-                            pill
-                            style={{
-                                color: "#00BFA6",
-                                borderColor: "#00BFA6",
-                                borderWidth: "1px",
-                                borderStyle: "solid",
-                            }}
-                            className=" mb-1"
-                            key={i}
-                        >
-                            {t}
-                            <a
-                                href="#"
-                                style={{
-                                    marginLeft: "15px",
-                                }}
-                                size="sm"
-                                type="submit"
-                                onClick={() => removeItem(i)}
-                                variant="secondary"
-                            >
-                                x
-                            </a>
-                        </Badge>
-                    ))}
-                    {!props.tech.length && "No technologies provided"}
-                </Modal.Footer>
                 <Modal.Footer>
                     <Button
                         className="w-100"
