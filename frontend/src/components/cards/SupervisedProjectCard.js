786897c78cff1aa37a26ab68a81ddb86cd555b55
salma27 <36279740+salma27@users.noreply.github.com>
2021-05-19 16:44:26 +0200

    leave team icon updated

--- frontend/src/components/cards/SupervisedProjectCard.js
+++ frontend/src/components/cards/SupervisedProjectCard.js
@@ -1,26 +1,31 @@
 import React from "react";
-import { Badge, Card } from "react-bootstrap";
-import { AiFillEdit, AiFillDelete } from "react-icons/ai";
+import { Badge, Card, Button, OverlayTrigger, Tooltip } from "react-bootstrap";
+import { ImExit } from "react-icons/im";
 
 const SupervisedProjectCard = ({ title, brief_description, tech = [] }) => {
+    const renderTooltip = (props) => (
+        <Tooltip id="button-tooltip" {...props}>
+            Leave Team
+        </Tooltip>
+    );
     return (
         <Card className="mb-3">
             <Card.Body>
                 <Card.Title className="d-flex">
                     <b>{title}</b>
                     <div className="ml-auto">
-                        <button
-                            className="btn btn-lg btn-primary py-1 px-2 mr-1"
-                            style={{
-                                backgroundColor: "#00BFA6",
-                                borderColor: "#00BFA6",
-                            }}
+                        <OverlayTrigger
+                            placement="right"
+                            delay={{ show: 0, hide: 0 }}
+                            overlay={renderTooltip}
                         >
-                            <AiFillEdit />
-                        </button>
-                        <button className="btn btn-lg btn-outline-danger py-1 px-2 mr-1">
-                            <AiFillDelete />
-                        </button>
+                            <Button
+                                variant="danger"
+                                className="btn btn-lg py-1 px-2 mr-1"
+                            >
+                                <ImExit />
+                            </Button>
+                        </OverlayTrigger>
                     </div>
                 </Card.Title>
                 <hr />
@@ -31,7 +36,6 @@ const SupervisedProjectCard = ({ title, brief_description, tech = [] }) => {
                         <Badge
                             pill
                             style={{
-                                // backgroundColor: "white",
                                 color: "#00BFA6",
                                 borderColor: "#00BFA6",
                                 borderWidth: "1px",
