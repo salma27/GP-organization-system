07b054e961adfe040b61b178396840f8a39b4552
salma27 <36279740+salma27@users.noreply.github.com>
2021-05-19 02:13:10 +0200

    Update SupervisedProjects.js

--- frontend/src/components/pages/SupervisedProjects/SupervisedProjects.js
+++ frontend/src/components/pages/SupervisedProjects/SupervisedProjects.js
@@ -1,11 +1,52 @@
 import { Button, Form, Col, Row } from "react-bootstrap";
 import React, { useState } from "react";
 import { BsButton } from "utils";
+import { ProjectCard } from "components/cards";
+import { CardColumns } from "react-bootstrap";
 
 function SupervisedProjects() {
+    const projects = [
+        {
+            title: "Tbdel",
+            brief_description:
+                "an online platform that people can exchange their old items together on",
+            tech: ["ML", "Web development", "Mobile app development"],
+        },
+        {
+            title: "GP organizer",
+            brief_description:
+                "A faculty platformfor student to register their ideas and form teams on",
+            tech: ["ML", "Web development", "Mobile app development"],
+        },
+        {
+            title: "Gold digger",
+            brief_description: "a gold stock pridector using ML",
+            tech: ["ML", "Web development"],
+        },
+        {
+            title: "Tbdel",
+            brief_description:
+                "an online platform that people can exchange their old items together on",
+            tech: ["Mobile app development"],
+        },
+        {
+            title: "Gold digger",
+            brief_description:
+                "Nullam fermentum quam interdum tortor fermentum auctor. Morbi in venenatis lectus. In dignissim iaculis nisi ultricies dictum. Aliquam erat volutpat",
+        },
+    ];
+
     return (
         <>
-            <div>hi</div>
+            <div>
+                <div className="row">
+                    <CardColumns>
+                        {projects.map((p, i) => (
+                            <ProjectCard {...p} key={i} />
+                        ))}
+                    </CardColumns>
+                </div>
+            </div>
         </>
     );
 }
