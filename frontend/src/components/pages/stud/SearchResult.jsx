9f376738b37b260a71d63284d1e8d31eb195a896
salma27 <36279740+salma27@users.noreply.github.com>
2021-07-11 22:39:48 +0200

    Update SearchResult.jsx

--- frontend/src/components/pages/stud/SearchResult.jsx
+++ frontend/src/components/pages/stud/SearchResult.jsx
@@ -8,7 +8,7 @@ const res = [
         name: "Sarah Saeed Ibrahim Rofail",
         department : "cs",
         tech: ["ML", "Web development", "Mobile app development"],
-        projects:["pro1", "pro2", "pro3", "pro4", "pro5"],
+        projects:["pro1", "pro2", "pro3", "pro4", "pro5", "pro6"],
         num : 3
     },
     {
