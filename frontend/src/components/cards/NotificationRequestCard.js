1fd3e56f3518530a8cf04c4a52c76c9bdb3d3be3
salma27 <36279740+salma27@users.noreply.github.com>
2021-07-13 01:37:59 +0200

    join requests notification list added

---
+++ frontend/src/components/cards/NotificationRequestCard.js
@@ -0,0 +1,72 @@
+import { SettingsBrightness } from "@material-ui/icons";
+import { React, useState } from "react";
+import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { studentProfileRoute } from "routes/routes";
+import { confirmAction } from "utils";
+
+const NotificationRequestCard = ({ name, join }) => {
+    const accept = () => {
+        confirmAction({
+            message: "Are you sure you want to accept this request?",
+            onConfirm: () => {},
+        });
+    };
+    const [hover, setHover] = useState(false);
+    const bg = hover
+        ? {
+              backgroundColor: "white",
+              borderColor: "#00BFA6",
+              color: "#00BFA6",
+          }
+        : {
+              backgroundColor: "#00BFA6",
+              borderColor: "#00BFA6",
+              color: "white",
+          };
+    const decline = () => {
+        confirmAction({
+            message: "Are you sure you want to decline this request?",
+            onConfirm: () => {},
+        });
+    };
+
+    return (
+        <Card.Body className="m-0 p-0">
+            <Card.Title className="m-0 p-0">
+                <Link to={studentProfileRoute}>
+                    <b>{name}</b>
+                </Link>
+            </Card.Title>
+            <Card.Text>
+                {join
+                    ? "This student wants you to join their team!"
+                    : "This supervisor wants to supervise your team!"}
+
+                <div className="d-flex justify-content-center and text-center">
+                    <button
+                        className="btn btn-primary py-0 px-1 mr-1"
+                        style={bg}
+                        onPointerOver={() => {
+                            setHover(true);
+                        }}
+                        onPointerOut={() => {
+                            setHover(false);
+                        }}
+                        onClick={accept}
+                    >
+                        Accept
+                    </button>
+                    <button
+                        className="btn btn-outline-danger py-0 px-1 mr-1"
+                        onClick={decline}
+                    >
+                        Decline
+                    </button>
+                </div>
+            </Card.Text>
+            <hr />
+        </Card.Body>
+    );
+};
+export default NotificationRequestCard;
--- frontend/src/components/navbar/NotificationDropdown.js
+++ frontend/src/components/navbar/NotificationDropdown.js
@@ -1,29 +1,37 @@
 import React, { useEffect, useState, Link } from "react";
 import { BsCheckCircle } from "react-icons/bs";
-// import { FormattedMessage } from "react-intl";
-import { useRequest } from "hooks";
-//import { clientSubInfo } from "requests";
-import moment from "moment";
 import { BsTrashFill } from "react-icons/bs";
-// import {FaTimes} from "react-icons/fa";
 import "./NotificationDropdown.css";
+import { RequestCard, NotificationRequestCard } from "components/cards";
+
 const NotificationDropdown = () => {
-    //const [getSubInfo] = useRequest(clientSubInfo);
-    //const [getSubInfo] = useRequest();
-    const [renewal, setRenewal] = useState(0);
-    const [planId, setPlanId] = useState("FREE");
-    /*
-    useEffect(() => {
-        getSubInfo()
-            .then((r) => {
-                const { renewal_date, plan_id } = r.data.data.subscriptions[0];
-                setRenewal(moment(renewal_date).diff(moment(), "days"));
-                setPlanId(plan_id);
-            })
-            .catch((e) => {})
-            .finally(() => {});
-    }, []);
-*/
+    const requests = [
+        {
+            name: "Alen douglas",
+            join: true,
+        },
+        {
+            name: "Mike mikey",
+            join: true,
+        },
+        {
+            name: "Ali kory",
+            join: false,
+        },
+        {
+            name: "My name",
+            join: false,
+        },
+        {
+            name: "Tarzan",
+            join: true,
+        },
+        {
+            name: "Elsa",
+            join: false,
+        },
+    ];
+
     return (
         <li className="nav-item dropdown no-arrow mx-1">
             <a
@@ -33,14 +41,10 @@ const NotificationDropdown = () => {
                 role="button"
                 data-toggle="dropdown"
                 data-target="#noti"
-                aria-haspopup="true"
             >
                 <i className="fas fa-bell fa-fw"></i>
-                {renewal <= 0 && (
-                    <span className="badge badge-danger badge-counter">
-                        new
-                    </span>
-                )}
+
+                <span className="badge badge-danger badge-counter">new</span>
             </a>
             <div
                 className="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
@@ -49,157 +53,15 @@ const NotificationDropdown = () => {
             >
                 <h6 className="dropdown-header" id="notifications">
                     Notifications
-                    {/*<FormattedMessage
-                        id="notifications"
-                        defaultMessage="Notifications"
-                    />*/}
                 </h6>
-                {/* <Link
-                    className="dropdown-item d-flex align-items-center"
-                    //to={editProfileRoute}
-                >
-                    <div className="mr-3">
-                        <div className="icon-circle bg-primary">
-                             <TiPencil className="text-white" size="1.5em" />
-                        </div>
-                    </div>
-                    <div className="text-gray-500">
-                         <FormattedMessage
-                            id="edit_profile"
-                            defaultMessage="Edit profile"
-                       />
-                    </div>
-                </Link>
-               */}
-                {renewal >= 0 && (
-                    <a
-                        className="dropdown-item d-flex align-items-center"
-                        href="#"
-                    >
-                        <div className="mr-3">
-                            {renewal <= 30 ? (
-                                <div className="icon-circle bg-danger">
-                                    <i className="fas fa-exclamation-triangle text-white" />
-                                </div>
-                            ) : (
-                                <div className="icon-circle bg-success">
-                                    <BsCheckCircle
-                                        className="text-white"
-                                        size="1.5em"
-                                    />{" "}
-                                </div>
-                            )}
-                        </div>
-                        <div>
-                            {planId === "FREE" ? (
-                                <div className="text-gray-500">
-                                    {/*  <FormattedMessage
-                                        id="all_set"
-                                        defaultMessage="You are all set"
-                                  />*/}
-                                </div>
-                            ) : (
-                                <div className="text-gray-500">
-                                    {/* <FormattedMessage
-                                        id="remaining"
-                                        defaultMessage="Remaining days until renewal:"
-                                    />{" "}
-                                    {renewal / (30 * 12) < 100 ? (
-                                        <b>{renewal}</b>
-                                    ) : (
-                                        <FormattedMessage
-                                            id="never"
-                                            defaultMessage="Never"
-                                        />
-                                    )} */}
-                                </div>
-                            )}
-                        </div>
-                    </a>
-                )}
-                {renewal < 0 && renewal >= -14 && (
-                    <a
-                        className="dropdown-item d-flex align-items-center"
-                        href="#"
-                    >
-                        <div className="mr-3">
-                            <div className="icon-circle bg-danger">
-                                <i className="fas fa-exclamation-triangle text-white" />
-                            </div>
-                        </div>
-                        <div>
-                            <div className="text-gray-500">
-                                {/* <FormattedMessage
-                                    id="remaining_til_expiration"
-                                    defaultMessage="Remaining days until expiration:"
-                                />{" "} */}
-                                <b>{renewal + 14}</b>
-                            </div>
-                        </div>
-                    </a>
-                )}
-                {renewal < -14 && renewal >= -45 && (
-                    <a
-                        className="dropdown-item d-flex align-items-center"
-                        href="#"
-                    >
-                        <div className="mr-3">
-                            <div className="icon-circle bg-danger">
-                                <BsTrashFill
-                                    className="text-white"
-                                    size="1.5em"
-                                />
-                            </div>
-                        </div>
-                        <div>
-                            <div className="text-gray-500">
-                                {/* <FormattedMessage
-                                    id="remaining_til_dataloss"
-                                    defaultMessage="Remaining days until data loss:"
-                                />{" "} */}
-                                <b>{renewal + 45}</b>
-                            </div>
-                        </div>
-                    </a>
-                )}
-            </div>
-        </li>
-    );
-};
-
-export default NotificationDropdown;
-
-/*
-import React from "react";
-
-const NotificationDropdown = () => {
-    return (
-        <li className="nav-item dropdown no-arrow mx-1">
-            <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                id="alertsDropdown"
-                role="button"
-                data-toggle="dropdown"
-                data-target="#noti"
-                aria-haspopup="true"
-            >
-                <i className="fas fa-bell fa-fw"></i>
-            </a>
-            <div
-                className="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                aria-labelledby="alertsDropdown"
-                id="noti"
-            >
-                <h6 className="dropdown-header">Notifications</h6>
-                <a className="dropdown-item d-flex align-items-center" href="#">
-                    <div className="mr-3">
-                        <div className="icon-circle bg-danger">
-                            <i className="fas fa-exclamation-triangle text-white" />
-                        </div>
-                    </div>
+                <hr />
+                <a className="dropdown-item " href="#">
                     <div>
-                        <div className="text-gray-500">Message</div>
+                        <h6 className="dropdown-header">Join Requests:</h6>
+                        <hr />
+                        {requests.map((r, i) => (
+                            <NotificationRequestCard {...r} key={i} />
+                        ))}
                     </div>
                 </a>
             </div>
@@ -208,4 +70,3 @@ const NotificationDropdown = () => {
 };
 
 export default NotificationDropdown;
-*/
