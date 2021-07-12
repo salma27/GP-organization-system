e1d5c180112dc17572b3f0c547e378491aa74042
Sara Samer <ss.titanic199@gmail.com>
2021-07-12 23:46:42 +0200

    Check that user is staff member to be able to view projects

--- frontend/src/components/pages/stud/Info.js
+++ frontend/src/components/pages/stud/Info.js
@@ -2,16 +2,19 @@ import React from "react";
 import { Navbar } from "components/navbar";
 import * as pages from "./";
 import { Tab, Tabs, Nav } from "react-bootstrap";
+import { useAuthContext } from "hooks";
+
 const style = {
     // backgrounds from 1 to 5 i.e. feed_4
     backgroundImage: "url(/feed_7.svg)",
     backgroundRepeat: "no-repeat",
     backgroundAttachment: "fixed",
-    backgroundSize: "cover",
+    backgroundSize: "cover"
     // height:"100vh",
 };
 
 const Info = () => {
+    const { isStaff, auth } = useAuthContext();
     return (
         <div className="container-fluid" style={style}>
             <div className="row">
@@ -32,9 +35,11 @@ const Info = () => {
                         <Tab eventKey="team" title="Team">
                             <pages.ShowingTeam />
                         </Tab>
-                        <Tab eventKey="projects" title="Projects">
-                            <pages.Projects />
-                        </Tab>
+                        {isStaff && (
+                            <Tab eventKey="projects" title="Projects">
+                                <pages.Projects />
+                            </Tab>
+                        )}
                     </Tabs>
                 </div>
             </div>
