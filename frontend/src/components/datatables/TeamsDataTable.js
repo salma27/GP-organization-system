7752f84b074a7797e92bc8709ef971d429529385
salma27 <36279740+salma27@users.noreply.github.com>
2021-07-13 22:52:59 +0200

    delete, edit, add icons added

--- frontend/src/components/datatables/TeamsDataTable.js
+++ frontend/src/components/datatables/TeamsDataTable.js
@@ -1,6 +1,12 @@
 import React from "react";
 import data from "components/data";
 import { DataTable } from "utils";
+import { IconButton } from "@material-ui/core";
+import { createTheme, createMuiTheme } from "@material-ui/core/styles";
+import AddIcon from "@material-ui/icons/Add";
+import EditIcon from "@material-ui/icons/Edit";
+import DeleteIcon from "@material-ui/icons/Delete";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
 
 const columns = [
     {
@@ -45,12 +51,82 @@ const columns = [
             filterType: "multiselect",
         },
     },
+    {
+        name: "Delete",
+        options: {
+            filter: true,
+            sort: false,
+            empty: true,
+            customBodyRender: (value, tableMeta, updateValue) => {
+                return (
+                    <DeleteIcon>
+                        <button
+                            onClick={() => {
+                                const { data } = this.state;
+                                data.shift();
+                                this.setState({ data });
+                            }}
+                        ></button>
+                    </DeleteIcon>
+                );
+            },
+        },
+    },
+    {
+        name: "Edit",
+        options: {
+            filter: true,
+            sort: false,
+            empty: true,
+            customBodyRender: (value, tableMeta, updateValue) => {
+                return (
+                    <EditIcon>
+                        <button
+                            onClick={() =>
+                                window.alert(
+                                    `Clicked "Edit" for row ${tableMeta.rowIndex}`
+                                )
+                            }
+                        ></button>
+                    </EditIcon>
+                );
+            },
+        },
+    },
 ];
+const getMuiTheme = () =>
+    createTheme({
+        overrides: {
+            MUIDataTableToolbar: {
+                actions: {
+                    display: "flex",
+                    flexDirection: "row",
+                    flex: "initial",
+                },
+            },
+        },
+    });
 
+const options = {
+    customToolbar: () => {
+        return (
+            <IconButton style={{ order: -1 }}>
+                <AddIcon />
+            </IconButton>
+        );
+    },
+};
 function TeamsDataTable() {
     return (
         <>
-            <DataTable title="Teams List" data={data} columns={columns} />
+            <MuiThemeProvider theme={getMuiTheme()}>
+                <DataTable
+                    options={options}
+                    title="Teams List"
+                    data={data}
+                    columns={columns}
+                />
+            </MuiThemeProvider>
         </>
     );
 }
