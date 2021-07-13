02796292d4aec5951671b7ebab279aaffa0d8871
salma27 <36279740+salma27@users.noreply.github.com>
2021-07-13 23:33:09 +0200

    delete multiple rows added

--- frontend/src/components/datatables/TeamsDataTable.js
+++ frontend/src/components/datatables/TeamsDataTable.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { React, useState } from "react";
 import data from "components/data";
 import { DataTable } from "utils";
 import { IconButton } from "@material-ui/core";
@@ -7,126 +7,132 @@ import AddIcon from "@material-ui/icons/Add";
 import EditIcon from "@material-ui/icons/Edit";
 import DeleteIcon from "@material-ui/icons/Delete";
 import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import MUIDataTable from "mui-datatables";
 
-const columns = [
-    {
-        name: "id",
-        label: "ID",
-        options: {
-            filter: false,
+function TeamsDataTable() {
+    const [state, setState] = useState(data);
+    const columns = [
+        {
+            name: "id",
+            label: "ID",
+            options: {
+                filter: false,
+            },
         },
-    },
-    {
-        name: "name",
-        label: "Name",
-        options: {
-            filterType: "textField",
+        {
+            name: "name",
+            label: "Name",
+            options: {
+                filterType: "textField",
+            },
         },
-    },
-    {
-        name: "email",
-        label: "Mail",
-        options: {
-            filterType: "textField",
+        {
+            name: "email",
+            label: "Mail",
+            options: {
+                filterType: "textField",
+            },
         },
-    },
-    {
-        name: "major",
-        label: "Department",
-        options: {
-            filterType: "checkbox",
+        {
+            name: "major",
+            label: "Department",
+            options: {
+                filterType: "checkbox",
+            },
         },
-    },
-    {
-        name: "gpa",
-        label: "GPA",
-        options: {
-            filter: false,
+        {
+            name: "gpa",
+            label: "GPA",
+            options: {
+                filter: false,
+            },
         },
-    },
-    {
-        name: "team",
-        label: "Team",
-        options: {
-            filterType: "multiselect",
+        {
+            name: "team",
+            label: "Team",
+            options: {
+                filterType: "multiselect",
+            },
         },
-    },
-    {
-        name: "Delete",
-        options: {
-            filter: true,
-            sort: false,
-            empty: true,
-            customBodyRender: (value, tableMeta, updateValue) => {
-                return (
-                    <DeleteIcon>
+        /*{
+            name: "Delete",
+            options: {
+                filter: true,
+                sort: false,
+                empty: true,
+                customBodyRender: (value, tableMeta, updateValue) => {
+                    return (
                         <button
                             onClick={() => {
-                                const { data } = this.state;
-                                data.shift();
-                                this.setState({ data });
+                                const tmpData = state;
+
+                                setState(tmpData.shift());
                             }}
-                        ></button>
-                    </DeleteIcon>
-                );
-            },
-        },
-    },
-    {
-        name: "Edit",
-        options: {
-            filter: true,
-            sort: false,
-            empty: true,
-            customBodyRender: (value, tableMeta, updateValue) => {
-                return (
-                    <EditIcon>
-                        <button
-                            onClick={() =>
-                                window.alert(
-                                    `Clicked "Edit" for row ${tableMeta.rowIndex}`
-                                )
-                            }
-                        ></button>
-                    </EditIcon>
-                );
-            },
-        },
-    },
-];
-const getMuiTheme = () =>
-    createTheme({
-        overrides: {
-            MUIDataTableToolbar: {
-                actions: {
-                    display: "flex",
-                    flexDirection: "row",
-                    flex: "initial",
+                        >
+                            <DeleteIcon />
+                        </button>
+                    );
                 },
             },
         },
-    });
+        {
+            name: "Edit",
+            options: {
+                filter: true,
+                sort: false,
+                empty: true,
+                customBodyRender: (value, tableMeta, updateValue) => {
+                    return (
+                        <EditIcon>
+                            <button
+                                onClick={() =>
+                                    window.alert(
+                                        `Clicked "Edit" for row ${tableMeta.rowIndex}`
+                                    )
+                                }
+                            ></button>
+                        </EditIcon>
+                    );
+                },
+            },
+        },*/
+    ];
 
-const options = {
-    customToolbar: () => {
-        return (
-            <IconButton style={{ order: -1 }}>
-                <AddIcon />
-            </IconButton>
-        );
-    },
-};
-function TeamsDataTable() {
+    /*const getMuiTheme = () =>
+        createTheme({
+            overrides: {
+                MUIDataTableToolbar: {
+                    actions: {
+                        display: "flex",
+                        flexDirection: "row",
+                        flex: "initial",
+                    },
+                },
+            },
+        });
+*/
+    const options = {
+        selectableRows: "multiple",
+        /* responsive: "stacked",
+        page: 2,
+        customToolbar: () => {
+            return (
+                <IconButton style={{ order: -1 }}>
+                    <AddIcon />
+                </IconButton>
+            );
+        },*/
+    };
     return (
         <>
-            <MuiThemeProvider theme={getMuiTheme()}>
-                <DataTable
-                    options={options}
-                    title="Teams List"
-                    data={data}
-                    columns={columns}
-                />
-            </MuiThemeProvider>
+            {/*<MuiThemeProvider theme={getMuiTheme()}>*/}
+            <MUIDataTable
+                options={options}
+                title="Teams List"
+                data={state}
+                columns={columns}
+            />
+            {/*</MuiThemeProvider>*/}
         </>
     );
 }
