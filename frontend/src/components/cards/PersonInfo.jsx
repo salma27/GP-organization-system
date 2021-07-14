967c688df0352c4950c793a6b39ed75711a72864
sarah105 <sara.said995@yahoo.com>
2021-07-14 02:10:36 +0200

    edit in PersonInfo style

--- frontend/src/components/cards/PersonInfo.jsx
+++ frontend/src/components/cards/PersonInfo.jsx
@@ -31,7 +31,7 @@ const PersonInfo = ({show=false,btn=false}) => {
                 <hr className="mt-1"/> 
                 </>
             }           
-            <div className="row container">
+            <div className="row container center-row m-0">
                 {/* <div className="col-2"> */}
                     {/* <figure className="figure col-12 ">
                        <img src="\profile.svg" className="figure-img img-fluid rounded mx-auto " alt="..."/>
@@ -39,15 +39,15 @@ const PersonInfo = ({show=false,btn=false}) => {
                    </figure> */}
                    
                 {/* </div> */}
-                <div className="col-12 col-lg-3 " style={{}}>
-                    <img src="\profile.svg" className=" mw-100 mh-100"/>
+                <div className="col-12 col-lg-3 center-row" style={{}}>
+                    <img src="\profile.svg" className=" mw-100 mh-100 photo"/>
                 </div>
                 <div className="col-12 col-lg-9 personinfo-body" style={{}}>
                     <div className="personInfo row">
                         <div className="col-12 col-lg-3 mb-1">
                             <label className="personInfo-hidder">Name</label>
                         </div>
-                        <div className="col-12 col-lg-9">
+                        <div className="col-11 m-auto col-lg-9 person-info-p">
                             <p className="personInfo-second-text">Sarah Saeed Ibrahim Rofail Sarah Saeed Ibrahim Rofail Sarah Saeed Ibrahim Rofail</p>
                         </div>
                     </div>
@@ -55,23 +55,28 @@ const PersonInfo = ({show=false,btn=false}) => {
                         <div className="col-12 col-lg-3 mb-1">
                             <label className="personInfo-hidder">Email</label>
                         </div>
-                        <div className="col-12 col-lg-9">
+                        <div className="col-11 m-auto col-lg-9 person-info-p">
                             <p className="personInfo-second-text"> sara.said1998@gmail.com</p>
                         </div>
                     </div>
-                    <div className="personInfo row ">
+                    <div className="personInfo row mb-0">
                         <div className="col-12 col-lg-3 mb-1">
                             <label className="personInfo-hidder">Department</label>
                         </div>
-                        <div className="col-12 col-lg-2">
-                            <p className="personInfo-second-text">CS</p>
-                        </div>
+                       {!show &&
+                            <div className="col-11 m-auto col-lg-9 person-info-p">
+                                <p className="personInfo-second-text">CS</p>
+                            </div>
+                        }
                         {show && (
                             <>
+                            <div className="col-11 m-auto col-lg-2 person-info-p">
+                                <p className="personInfo-second-text">CS</p>
+                            </div>
                                 <div className="col-12 col-lg-4 mb-1">
                                     <label className="personInfo-hidder">Number Of Teams</label>
                                 </div>
-                                <div className="col-12 col-lg-3">
+                                <div className="col-11 m-auto col-lg-3 person-info-p">
                                     <p className="personInfo-second-text">5</p>
                                 </div>
                             </>
--- frontend/src/components/cards/ShowingNames.js
+++ frontend/src/components/cards/ShowingNames.js
@@ -7,19 +7,19 @@ const ShowingName = (props) => {
     return ( 
         <div>
             <h5 className="personInfo-hidder w-fit-mb">{props.title}</h5>
-                <div>
-                    {props.data.map((t, i) => (
-                        <Link to={r.userInfo} key={i}>
-                            <Badge
-                                pill
-                                className="mr-1 mb-1 name-link"
-                            >
-                                {t}
-                            </Badge>
-                        </Link>
-                        
-                    ))}
-                </div>
+            <div>
+                {props.data.map((t, i) => (
+                    <Link to={r.userInfo} key={i}>
+                        <Badge
+                            pill
+                            className="mr-1 mb-1 name-link"
+                        >
+                            {t}
+                        </Badge>
+                    </Link>
+                    
+                ))}
+            </div>
         </div>
     );
 }
