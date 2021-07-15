01003514710acd794b180ea8b39b6e8b02414a8e
salma27 <36279740+salma27@users.noreply.github.com>
2021-07-15 22:01:17 +0200

    dummy data added

--- frontend/src/components/datatables/RulesDataTable.js
+++ frontend/src/components/datatables/RulesDataTable.js
@@ -2,9 +2,8 @@ import { Button, Form, Col, Row } from "react-bootstrap";
 import React, { useState } from "react";
 import MUIDataTable from "mui-datatables";
 import { data } from "./../data/data";
-import { AdminNavbar } from "components/navbar";
 import { DataTable } from "utils";
-
+/*
 const columns = [
     { name: "ID", options: { filter: "textfield" } },
     { name: "Name", options: { filter: "textfiled" } },
@@ -13,8 +12,23 @@ const columns = [
     { name: "GPA", options: { filter: false } },
     { name: "Team", options: { filterType: "multiselect" } },
 ];
-
+*/
 function RulesDataTable() {
+    const data = [
+        ["CS", "3", "5", "1", "2", "1", "1"],
+        ["IS", "2", "5", "1", "2", "1", "1"],
+        ["IT", "4", "5", "1", "2", "1", "1"],
+        ["DS", "1", "5", "2", "3", "1", "1"],
+    ];
+    const columns = [
+        { name: "Department", options: { filterType: "checkbox" } },
+        { name: "Min students per team" },
+        { name: "Max students per team" },
+        { name: "Min doctors to supervise" },
+        { name: "Max doctors to supervise" },
+        { name: "Min TAs per team" },
+        { name: "Max TAs per team" },
+    ];
     return (
         <>
             <DataTable title={"Students List"} data={data} columns={columns} />
