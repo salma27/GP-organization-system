61bbe97bae37f2d8509b59ea0daa224ecaa769d5
salma27 <36279740+salma27@users.noreply.github.com>
2021-07-15 19:34:36 +0200

    adminRulesCard modified

---
+++ frontend/src/components/cards/AdminRulesCard.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import { Badge, Card, Button, OverlayTrigger, Tooltip } from "react-bootstrap";
+
+const AdminRulesCard = (props) => {
+    return (
+        <Card className="mb-3">
+            <Card.Body>
+                <button>hh</button>
+            </Card.Body>
+        </Card>
+    );
+};
+export default AdminRulesCard;
