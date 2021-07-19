9b9f138d0e0271afa3922cad59f43107b02905ed
sarah105 <sara.said995@yahoo.com>
2021-07-19 21:36:48 +0200

    add news feed

--- frontend/src/requests/admin/index.js
+++ frontend/src/requests/admin/index.js
@@ -3,3 +3,4 @@ export { adminAddOldProjects } from "./adminOldProjectsRequest";
 export { adminEditOldProjects } from "./adminOldProjectsRequest";
 export { adminDeleteOldProjects} from "./adminOldProjectsRequest";
 export { adminGetFeeds } from "./adminFeeds";
+export { adminAddFeed } from "./adminFeeds";
