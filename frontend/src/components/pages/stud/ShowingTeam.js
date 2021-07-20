5a4b3773b927748314c1fdea49f93532ded651bb
salma27 <36279740+salma27@users.noreply.github.com>
2021-07-21 01:15:03 +0200

    not finished yet

--- frontend/src/components/pages/stud/ShowingTeam.js
+++ frontend/src/components/pages/stud/ShowingTeam.js
@@ -1,11 +1,12 @@
-import {React,useState} from "react";
-import {ShowingNames,Technologies} from "components/cards";
-import {RiMailSendLine} from "react-icons/ri";
-import { useAuthContext } from "hooks";
+import { React, useEffect, useState } from "react";
+import { ShowingNames, Technologies } from "components/cards";
+import { RiMailSendLine } from "react-icons/ri";
+import { useAuthContext, useRequest } from "hooks";
 import { confirmAction } from "utils";
 import AskToJoinMyTeam from "components/Modals/AskToJoinMyTeam";
+import { toast } from "react-toastify";
 
-const projects=["pro1", "pro4", "pro5"];
+const projects = ["pro1", "pro4", "pro5"];
 
 const style = {
     // backgrounds from 1 to 5 i.e. feed_4
@@ -15,10 +16,24 @@ const style = {
     backgroundSize: "cover",
     // height:"100vh",
 };
-const students = ["sara","salma"]
+const students = ["sara", "salma"];
 const ShowinTeam = () => {
     const { isStaff } = useAuthContext();
     const [showModal, setShowModal] = useState(false);
+
+    const [request, requesting] = useRequest();
+    const [user, setUser] = useState([]);
+
+    useEffect(() => {
+        request({})
+            .then((r) => {
+                setUser(r.data);
+                toast.success("data loaded successfully");
+            })
+            .catch((e) => {
+                toast.error("Error showing team information");
+            });
+    }, []);
 
     const confirm = () => {
         confirmAction({
@@ -27,41 +42,58 @@ const ShowinTeam = () => {
         });
     };
 
-    return ( 
+    return (
         <div className="container-fluid" style={style}>
-            <div className="row m-auto" style={{maxWidth:"100%"}}>
+            <div className="row m-auto" style={{ maxWidth: "100%" }}>
                 <div className="col-12 personinfo-block">
                     <div className="row team-header">
                         <div className="col-12 col-lg-8">
-                            <h2>Team Name</h2>  
+                            <h2>Team Info</h2>
                         </div>
                         <div className="col-12 col-lg-4">
-                            <button className="primary-btn py-1 px-2 mr-1 mb-1" onClick={()=>isStaff?setShowModal(true):confirm()}>
-                                <RiMailSendLine className="mr-1"/>{isStaff?"Ask To Be Supervisor":"Ask to join my team"}
+                            <button
+                                className="primary-btn py-1 px-2 mr-1 mb-1"
+                                onClick={() =>
+                                    isStaff ? setShowModal(true) : confirm()
+                                }
+                            >
+                                <RiMailSendLine className="mr-1" />
+                                {isStaff
+                                    ? "Ask To Be Supervisor"
+                                    : "Ask to join my team"}
                             </button>
                             <AskToJoinMyTeam
                                 show={showModal}
                                 hide={() => setShowModal(false)}
                                 projects={projects}
-        
                             />
                         </div>
-                        
                     </div>
-                    
+
                     <hr />
-                    <ShowingNames title="Students" data={students} isStudent={true}/>
+                    <ShowingNames
+                        title="Students"
+                        data={students}
+                        isStudent={true}
+                    />
                     <hr />
-                    <ShowingNames title="Supervising doctors" data={students} isStudent={false}/>
+                    <ShowingNames
+                        title="Supervising doctors"
+                        data={students}
+                        isStudent={false}
+                    />
                     <hr />
-                    <ShowingNames title="Supervising Teaching Assistants" data={students} isStudent={false}/>
+                    <ShowingNames
+                        title="Supervising Teaching Assistants"
+                        data={students}
+                        isStudent={false}
+                    />
                     <hr />
-                    <Technologies tech={students}/>
-                    
+                    <Technologies tech={students} />
                 </div>
             </div>
         </div>
     );
-}
- 
+};
+
 export default ShowinTeam;
