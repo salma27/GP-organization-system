65bcb00e93e1754d498903f196657111270e4ec8
salma27 <36279740+salma27@users.noreply.github.com>
2021-07-22 23:29:35 +0200

    Update Notes.js

--- frontend/src/components/StudentProfile/Notes.js
+++ frontend/src/components/StudentProfile/Notes.js
@@ -1,16 +1,35 @@
+import { useRequest } from "hooks";
 import React, { useState } from "react";
 import { Form } from "react-bootstrap";
+import { toast } from "react-toastify";
+import editStudentProfile from "requests/editStudentProfile";
 import { BsButton } from "utils";
 import "./Notes.css";
 
 function Notes(props) {
-    const [note, setNote] = useState(props.info.bio);
+    const [note, setNote] = useState({ value: props.info.bio });
+    const [request, requesting] = useRequest(editStudentProfile);
+    const editProfile = (event) => {
+        event.preventDefault();
+        request({ bio: note })
+            .then((r) => {
+                toast.success("Updated successully");
+            })
+            .catch((e) => {
+                toast.error("Error updating profile");
+            });
+    };
+    const handleChange = ({ target: { name, value } }) => {
+        setNote({ ...note, [name]: value });
+    };
+    console.log(note.value);
     return (
         <>
-            <Form onSubmit={(e) => e.preventDefault()} className="w-100">
+            <Form className="w-100">
                 <Form.Group>
                     <Form.Label>Notes: </Form.Label>
                     <Form.Control
+                        autoFocus
                         as="textarea"
                         rows={3}
                         style={{
@@ -18,10 +37,15 @@ function Notes(props) {
                             backgroundColor: "#e9ecef",
                             opacity: 1,
                         }}
-                        value={note}
-                        onChange={(e) => {
+                        value={props.info.bio}
+                        name="value"
+                        id="value"
+                        onChange={handleChange}
+                        /*(e) => {
+                            setNote(props.info.bio);
                             setNote(e.target.value);
-                        }}
+                            console.log(note);
+                        }}*/
                     />
                 </Form.Group>
                 <BsButton
@@ -29,6 +53,7 @@ function Notes(props) {
                     id="notesBtn"
                     width="300px"
                     label="Add Notes"
+                    onClick={editProfile}
                 />
             </Form>
         </>
