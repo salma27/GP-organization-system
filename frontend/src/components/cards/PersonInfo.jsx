3f4c737aafce51ed42c50b496c4823132435d370
salma27 <36279740+salma27@users.noreply.github.com>
2021-07-22 10:31:59 +0200

    Revert "Revert "...""
    This reverts commit 8a770147784a77b7d0e2a8f68737f49f662d20f5.

--- frontend/src/components/cards/PersonInfo.jsx
+++ frontend/src/components/cards/PersonInfo.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./../../css/personInfoCard.css";
 import {RiMailSendLine} from "react-icons/ri"
 import { confirmAction } from "utils";
 import { useAuthContext } from "hooks";
+import { AskToJoinMyTeam } from 'components/Modals';
 
 const PersonInfo = ({show=false,btn=false, info}) => {
-    const { isStaff } = useAuthContext();
-
+    //const { isStaff } = useAuthContext();
+    const [showModal, setShowModal] = useState(false);
+    const [projects, setProjects] = useState([]);
     const confirm = () => {
         confirmAction({
             message: "Are you sure you want to send this request?",
@@ -15,35 +17,27 @@ const PersonInfo = ({show=false,btn=false, info}) => {
     };
     const takenTeams = info.teams?info.teams.length:0;
     return ( 
-        // <div className="personInfoCard row">
-        //     <figure className="figure col-12 personImg ">
-        //         <img src="\profile.svg" className="figure-img img-fluid rounded mx-auto " alt="..."/>
-        //         <figcaption className="figure-caption text-center">Sarah Saeed Ibrahim Rofail</figcaption>
-        //     </figure>
-        // </div>
         <div className="personinfo-block" >
-            {(!isStaff && btn) &&
-                <>
-                <div className="left-link">
-                    <button className="btn primary-btn py-1 px-2 mr-1 mb-1 w-auto" onClick={confirm}>
-                        <RiMailSendLine className="mr-1"/>Ask To Be a Supervisor
-                    </button>
-                </div>
+           
+            <div className="left-link">
+                <button className="btn primary-btn py-1 px-2 mr-1 mb-1 w-auto" onClick={show?setShowModal(true):confirm}>
+                    <RiMailSendLine className="mr-1"/>{show?"Ask To Be a Supervisor":"Ask to join team"}
+                </button>
+                    
+                <AskToJoinMyTeam
+                    show={showModal}
+                    hide={() => setShowModal(false)}
+                    projects={projects}  
+                />
                 <hr className="mt-1"/> 
-                </>
-            }           
+            </div>
+
             <div className="row container center-row m-0">
-                {/* <div className="col-2"> */}
-                    {/* <figure className="figure col-12 ">
-                       <img src="\profile.svg" className="figure-img img-fluid rounded mx-auto " alt="..."/>
-                       {/* <figcaption className="figure-caption text-center">Sarah Saeed Ibrahim Rofail</figcaption> 
-                   </figure> */}
-                   
-                {/* </div> */}
-                <div className="col-12 col-lg-3 center-row" style={{}}>
+                
+                <div className="col-12 col-lg-3 center-row" >
                     <img src="\profile.svg" className=" mw-100 mh-100 photo"/>
                 </div>
-                <div className="col-12 col-lg-9 personinfo-body" style={{}}>
+                <div className="col-12 col-lg-9 personinfo-body" >
                     <div className="personInfo row">
                         <div className="col-12 col-lg-3 mb-1">
                             <label className="personInfo-hidder">Name</label>
@@ -80,7 +74,6 @@ const PersonInfo = ({show=false,btn=false, info}) => {
                     </div>
                 </div>
             </div>
-            {/* <hr /> */}
             
         </div>
     );
